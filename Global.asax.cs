d9618ab6d27467ef09022714ed8a353a2bc14810
seydazinnur <34726875+seydazinnur@users.noreply.github.com>
2020-05-23 13:24:50 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using _20200521_Odev.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace _20200521_Odev
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
